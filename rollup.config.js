897c9c3157eee1e5b1c6a82eb6c1491eaa268b66
mcanam <mcanam910@gmail.com>
2021-10-31 21:40:21 +0700

    update rollup  config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import styles from "rollup-plugin-styles";
+import replace from "@rollup/plugin-replace";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import { babel } from "@rollup/plugin-babel";
 import pkg from "./package.json";
@@ -9,43 +11,72 @@ const header = `
 /*!
  * ${pkg.name} v${pkg.version}
  * ${pkg.description}
- * ${pkg.homepage.replace(/#readme/, "")}
+ * ${pkg.homepage}
  * ${pkg.license} license by ${pkg.author}
  */
 `;
 
-let config = {
-    input: "src/iblize.js",
-    output: {
-        file: "dist/iblize.js",
-        format: "umd",
-        name: "Iblize",
-        banner: header,
-        sourcemap: false
-    },
-    plugins: [
-        resolve(),
-        commonjs(),
-        styles({
-            mode: ["inject", { 
-                prepend: true, 
-                attributes: { id: "iblize_style" }
-            }],
-            minimize: true
-        })
-    ]
+const dev = {
+      input: "src/iblize.js",
+      output: {
+            file: ".dev-build/iblize.js",
+            format: "umd",
+            name: "Iblize",
+            sourcemap: true
+      },
+      plugins: [
+            resolve(),
+            commonjs(),
+            replace({
+                  preventAssignment: true,
+                  __VERSION__: `"${pkg.version}"`
+            }),
+            babel({
+                  babelHelpers: "bundled",
+                  exclude: "node_modules/**",
+                  presets: ["@babel/preset-env"]
+            }),
+            serve({
+                  port: 4321,
+                  open: true
+            }),
+            livereload()
+      ]
 };
 
-if (process.env.mode == "prod") {
-    config.output.sourcemap = true;
-    config.plugins.push(
-        babel({
-            babelHelpers: "bundled",
-            presets: ["@babel/preset-env"],
-            // plugins: [["prismjs", {}]]
-        }),
-        terser()
-    );
-}
+const prod = {
+      input: "src/iblize.js",
+      output: [
+            {
+                  file: "dist/iblize.js",
+                  format: "umd",
+                  name: "Iblize",
+                  banner: header,
+                  sourcemap: true
+            },
+            {
+                  file: "dist/iblize.min.js",
+                  format: "umd",
+                  name: "Iblize",
+                  banner: header,
+                  sourcemap: true,
+                  plugins: [ terser() ]
+            }
+      ],
+      plugins: [
+            resolve(),
+            commonjs(),
+            replace({
+                  preventAssignment: true,
+                  __VERSION__: `"${pkg.version}"`
+            }),
+            babel({
+                  babelHelpers: "bundled",
+                  exclude: "node_modules/**",
+                  presets: ["@babel/preset-env"]
+            })
+      ]
+};
 
+let config = process.env.MODE == "prod" ? prod : dev;
 export default config;
